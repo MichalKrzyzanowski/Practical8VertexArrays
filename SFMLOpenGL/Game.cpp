787ae32368d4773717693a20b6d20cb884b3406b
(Student) - Michael Krzyzanowski <c00240696@ITCARLOW.IE>
2020-01-14 10:08:23 +0000

    added remaining points, added vector3 points for calculations, added rotations, scaling and translations

--- SFMLOpenGL/Game.cpp
+++ SFMLOpenGL/Game.cpp
@@ -3,7 +3,7 @@
 bool flip = false;
 int current = 1;
 
-Game::Game() : window(VideoMode(800, 600), "OpenGL Cube")
+Game::Game() : window(sf::VideoMode(800, 600), "OpenGL Cube")
 {
 
 }
@@ -12,28 +12,57 @@ Game::~Game() {}
 
 
 // Vertices for one Triangle
-float vertices[] = { 1.0f, 1.0f, -5.0f, -1.0f, 1.0f, -5.0f, -1.0f, -1.0f, -5.0f };
+float vertices[24] = 
+			{ -1.0f, -1.0f, 1.0f ,
+			   1.0f, -1.0f, 1.0f,
+		  	  1.0f, 1.0f, 1.0f,
+			   -1.0f, 1.0f, 1.0f,
+
+			  -1.0f, -1.0f, -1.0f,
+			   1.0f, -1.0f, -1.0f,
+			  1.0f, 1.0f, -1.0f,
+			   -1.0f, 1.0f, -1.0f };
+
+Vector3 m_points[] = { { vertices[0], vertices[1] , vertices[2] },
+						{ vertices[3], vertices[4] , vertices[5] },
+						{ vertices[6], vertices[7] , vertices[8] },
+						{ vertices[9], vertices[10] , vertices[11] },
+						{ vertices[12], vertices[13] , vertices[14] },
+						{ vertices[15], vertices[16] , vertices[17] },
+						{ vertices[18], vertices[19] , vertices[20] },
+						{ vertices[21], vertices[22] , vertices[23] } };
 
 // Colors for those vertices
 float colors[] = { 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f };
 
 // Index to be drawn
-unsigned int vertex_index[] = { 0, 1, 2 };
+unsigned int vertex_index[] = { 0,1,2,
+								2,3,0,
+								2,6,3,
+								6,7,3,
+								7,6,5,
+								5,4,7,
+								5,0,4,
+								5,1,0,
+								1,5,6,
+								6,2,1,
+								4,0,3,
+								3,7,4 };
 
 void Game::run()
 {
 
 	initialize();
 
-	Event event;
+	sf::Event event;
 
 	while (isRunning) {
 
-		cout << "Game running..." << endl;
+		std::cout << "Game running..." << std::endl;
 
 		while (window.pollEvent(event))
 		{
-			if (event.type == Event::Closed)
+			if (event.type == sf::Event::Closed)
 			{
 				isRunning = false;
 			}
@@ -44,27 +73,144 @@ void Game::run()
 
 }
 
+void Game::controlCube()
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::RotationX(rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::RotationX(-rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::RotationY(rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::RotationY(-rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::RotationZ(rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::E))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::RotationZ(-rotationAngle) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::Translate(0, -1) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::Translate(0, 1) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::Translate(-1, 0) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::Translate(1, 0) * m_points[i];
+		}
+	}
+
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Z))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::Scale3D(101.0) * m_points[i];
+		}
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::X))
+	{
+		for (int i = 0; i < 8; i++)
+		{
+			m_points[i] = Matrix3::Scale3D(99.0) * m_points[i];
+		}
+	}
+
+	for (int i = 0, j = 0; i < 8, j < 24; i++)
+	{
+		vertices[j] = m_points[i].X;
+		j++;
+		vertices[j] = m_points[i].Y;
+		j++;
+		vertices[j] = m_points[i].Z;
+		j++;
+	}
+}
+
 void Game::initialize()
 {
 	isRunning = true;
 
 	//glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glTranslatef(0.0f, 0.0f, -8.0f);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	gluPerspective(45.0, window.getSize().x / window.getSize().y, 1.0, 500.0);
 	glMatrixMode(GL_MODELVIEW);
+
+	glEnable(GL_CULL_FACE);
 }
 
 void Game::update()
 {
 	elapsed = clock.getElapsedTime();
 
-	cout << "Update up" << endl;
+	controlCube();
+
+	std::cout << "Update up" << std::endl;
 }
 
 void Game::render()
 {
-	cout << "Drawing" << endl;
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	std::cout << "Drawing" << std::endl;
 
 	glEnableClientState(GL_VERTEX_ARRAY);
 	glEnableClientState(GL_COLOR_ARRAY);
@@ -73,9 +219,9 @@ void Game::render()
 	glVertexPointer(3, GL_FLOAT, 0, &vertices);
 	glColorPointer(3, GL_FLOAT, 0, &colors);
 
-	//glDrawArrays(GL_TRIANGLES, 0, 3);
+	glDrawArrays(GL_TRIANGLES, 0, 3);
 
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, &vertex_index);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, &vertex_index);
 
 	glDisableClientState(GL_COLOR_ARRAY);
 	glDisableClientState(GL_VERTEX_ARRAY);
@@ -86,6 +232,6 @@ void Game::render()
 
 void Game::unload()
 {
-	cout << "Cleaning up" << endl;
+	std::cout << "Cleaning up" << std::endl;
 }
 
